4d6d5d0f481e56d275409ec1d98c5222c47f6208
fratim <frtim@student.ethz.ch>
2022-01-21 14:59:26 +0000

    new assist environment wrapper implemented and tested. Both single-agent emulation and two-agent environment can be run from demo.py file.

---
+++ minerl/herobraine/wrappers/assisst_agent_emulation_wrapper.py
@@ -0,0 +1,35 @@
+__author__ = "Tim Franzmeyer"
+__email__ = "tfranzmeyer [at] gmail [dot] com"
+
+import threading
+import gym
+
+
+class AssistWrapper(gym.Wrapper):
+
+    def __init__(self, env):
+        super().__init__(env)
+        self.env = env
+        self.lead_agent = "agent_0" # this agent is emulated within this wrapper
+        self.assist_agent = "agent_1"
+
+        self.action_space = self.env.action_space[self.assist_agent]
+
+    def get_lead_agent_action(self):
+        return self.env.action_space[self.lead_agent].sample()
+
+    def step(self, action_in_assist_agent):
+
+        lead_agent_action = self.get_lead_agent_action()
+
+        multi_action = {
+            self.lead_agent: lead_agent_action,
+            self.assist_agent: action_in_assist_agent
+        }
+
+        obs, rew, done, info = self.env.step(multi_action)
+        return obs[self.assist_agent], rew[self.assist_agent], done, info[self.assist_agent]
+
+    def reset(self):
+        multi_obs = self.env.reset()
+        return multi_obs[self.assist_agent]
